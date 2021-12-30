1a686cc1961e19e97a8052d0593eb1ccb90e7ab3
cdr4eelz <1408777+cdr4eelz@users.noreply.github.com>
2021-12-30 02:27:37 -0800

    Version bump 0.0.34

--- serialhub/_frontend.py
+++ serialhub/_frontend.py
@@ -9,4 +9,4 @@ Information about the frontend package of the widgets.
 """
 
 module_name = "serialhub" #pylint: disable=invalid-name
-module_version = "^0.0.33"  # pylint: disable=invalid-name
+module_version = "^0.0.34"  # pylint: disable=invalid-name
